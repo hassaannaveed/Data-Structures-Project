6067d6105ca67cb1ae483b3420a4ee15cef2b865
Hassaan Naveed <55402852+hassaannaveed@users.noreply.github.com>
2024-12-11 23:13:26 +0100

    Update B1.py
    Changed from Adjecency List to Adjecency Matrix as required in the question

--- B1.py
+++ B1.py
@@ -1,39 +1,41 @@
 from tabulate import tabulate
 
-def Task1(filename):
-
+#filename is the name of the file to be read
+#num is the total number of nodes
+def Task1(filename, num):
     try:
         with open(filename, "r") as file:
             lines = file.readlines()
     except FileNotFoundError:
         print("Sorry! Find not found.")
+        return None
 
     else:
         cleaned_lines = [line for line in lines if line.strip()]
 
-        adj_list = {}
+        #adj_list = {}
+        adj_matrix = [[0 for _ in range(num)] for _ in range(num)]
 
         nodes = cleaned_lines[0].split()
+        node_to_index = {node: i for i, node in enumerate(nodes)}
         for line in cleaned_lines[1:]:
             parts = line.split()
             node = parts[0]
             edges = list(map(int, parts[1:]))
 
-        
-            adj_list[node] = {nodes[i]: edges[i] for i in range(len(edges)) if edges[i] != 0}
+            row = node_to_index[node]
+            for col, weight in enumerate(edges):
+                adj_matrix[row][col] = weight
+        data = []
+        for i in range(num):
+            data.append([nodes[i]] + adj_matrix[i])
 
-    
-        #for node, neighbors in adj_list.items():
-            #print(node + ": " + str(neighbors))
-    
-        data = [[node, neighbors] for node, neighbors in adj_list.items()]
-        headers = ["Start", "End"]
+        headers = ["Node"] + nodes
 
         print(tabulate(data, headers=headers, tablefmt="fancy_grid"))
-    
-        return adj_list
 
+        return adj_matrix
 
 
 fn = "graph_directed_weighted.txt"
-adjacency_list = Task1(fn)
+adjacency_matrix = Task1(fn,10)
