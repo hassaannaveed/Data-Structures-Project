a8c86fe5eb308a143406726245efb37c8536ffef
Hassaan Naveed <55402852+hassaannaveed@users.noreply.github.com>
2024-12-13 16:42:24 +0100

    Revert back changes: No need for user input

--- B1.py
+++ B1.py
@@ -1,22 +1,19 @@
 from tabulate import tabulate
 
 #filename is the name of the file to be read
-#num is the total number of nodes
-def task1(filename, num):
+def task1(filename):
     try:
         with open(filename, "r") as file:
             lines = file.readlines()
     except FileNotFoundError:
         print("Sorry! Find not found.")
         return None
-
     else:
         cleaned_lines = [line for line in lines if line.strip()]
 
-        #adj_list = {}
-        adj_matrix = [[0 for _ in range(num)] for _ in range(num)]
-
         nodes = cleaned_lines[0].split()
+
+        adj_matrix = [[0 for _ in range(len(nodes))] for _ in range(len(nodes))]
         node_to_index = {node: i for i, node in enumerate(nodes)}
         for line in cleaned_lines[1:]:
             parts = line.split()
@@ -27,7 +24,7 @@ def task1(filename, num):
             for col, weight in enumerate(edges):
                 adj_matrix[row][col] = weight
         data = []
-        for i in range(num):
+        for i in range(len(nodes)):
             data.append([nodes[i]] + adj_matrix[i])
 
         headers = ["Node"] + nodes
@@ -36,6 +33,6 @@ def task1(filename, num):
 
         return adj_matrix
 
-count = int(input("Enter the Total number of Nodes: "))
+
 fn = "graph_directed_weighted.txt"
-adjacency_matrix = task1(fn,count)
+adjacency_matrix = task1(fn)
